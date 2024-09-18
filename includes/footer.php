2463862093d1e42de1d6b982deab1ff04b8c46ef
Georges Laerche <georges.laerche@gmail.com>
2024-09-18 14:48:10 +0200

    Utilisation de plusieurs fichiers PHP

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div>
+        <p>2011 / 2025 © Tous droits réservés</p>                
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">  
-    <script defer src="public/js/index.js"></script>
+    <?php require_once 'includes/head.php' ?>
     <title>Todo</title>
 </head>
 <body>
     <div class="container">
-        <header>
-            <div class="logo">Mon todo</div>
-        </header>
+        <?php require_once 'includes/header.php' ?>
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
@@ -22,11 +14,7 @@
                 <div class="todo-list"></div>
             </div>
         </div>
-        <footer>
-            <div>
-                <p>2011 / 2025 © Tous droits réservés</p>                
-            </div>
-        </footer>
+        <?php require_once 'includes/footer.php' ?>
     </div>
 </body>
 </html>
