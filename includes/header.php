d88654c8104a7255ceb6dc1724148293794dcc2b
Georges Laerche <georges.laerche@gmail.com>
2024-09-23 13:21:50 +0200

    mise en place du header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
+
 <header>
-    <a href="/" class="logo">The Blog</a>
+    <a href="/" class="logo">Blog</a>
+        <ul class="header-menu">
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+                
+            <a href="/add-article.php">Écrire un article</a>
+            </li>
+        </ul>
 </header>
